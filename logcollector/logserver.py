b669cd0f37db4acdf2c8c7659eac01f93f189135
Stuart Berg <bergs@janelia.hhmi.org>
2017-05-24 14:39:45 -0400

    logserver: Added debugging code for using the eclipse debugger.

--- logcollector/logserver.py
+++ logcollector/logserver.py
@@ -143,6 +143,41 @@ def flush_all():
     for f in open_files.values():
         f.flush()
 
+
+##
+## ECLIPSE DEBUGGING CODE
+##
+def connect_debugger():
+    import sys
+    import os
+    
+    # Possible paths to the pydev debugger module on your hard drive.
+    # Developers: Add your dev machine's pydev directory to this list.
+    pydev_src_paths = [ "/Applications/eclipse/plugins/org.python.pydev_4.5.5.201603221110/pysrc/",
+                        "/usr/local/eclipse/plugins/org.python.pydev_4.2.0.201507041133/pysrc/",
+                        '/Users/bergs/.p2/pool/plugins/org.python.pydev_5.5.0.201701191708/pysrc/' ]
+
+    pydev_src_paths = filter(os.path.exists, pydev_src_paths)
+    
+    if not pydev_src_paths:
+        raise RuntimeError("Error: Couldn't find the path to the pydev module.  You can't use PYDEV_DEBUGGER_ENABLED.")
+    
+    if len(pydev_src_paths) > 1:
+        raise RuntimeError("Error: I found more than one pydev module.  I don't know which one to use.")
+    
+    sys.path.append(pydev_src_paths[0])
+    import pydevd
+    print "Waiting for PyDev debugger..."
+    pydevd.settrace(stdoutToServer=True, stderrToServer=True, suspend=False)
+
+if int(os.getenv('PYDEV_DEBUGGER_ENABLED', 0)):
+    connect_debugger()
+
+##
+##
+##
+
+
 if __name__ == '__main__':
     import argparse
     import sys
